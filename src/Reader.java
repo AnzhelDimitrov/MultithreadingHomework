ae4dd652e54753fdd4633bd21fa6ecac5bab3c1f
AnzhelDimitrov <108436278+AnzhelDimitrov@users.noreply.github.com>
2024-07-04 00:00:34 +0300

    Storing file's words and their occurrences in a hashmap

--- src/Reader.java
+++ src/Reader.java
@@ -2,14 +2,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Reader {
 
     public static void fileReader() throws IOException {
-
+        HashMap<String, Integer> duplicateWords = new HashMap<>();
         try {
             File fileFolder = new File("D:\\JavaHomework\\MultithreadingAdv\\src\\resources");
             File[] files = fileFolder.listFiles();
+            ArrayList<String> wordsList = new ArrayList<>();
 
             assert files != null;
             for (File file : files) {
@@ -17,11 +21,25 @@ public class Reader {
                 String line;
 
                 while ((line = bufferd.readLine()) != null) {
-                    System.out.println(line);
+                    wordsList.addAll(Arrays.asList(line.split("[\\s.,;]+")));
+                    for (String word : wordsList) {
+                        if (duplicateWords.containsKey(word)) {
+                            int value = duplicateWords.get(word);
+                            duplicateWords.put(word, value + 1);
+                        } else {
+                            duplicateWords.put(word, 1);
+                        }
+                    }
                 }
             }
+
         } catch (Exception e) {
             e.getStackTrace();
+        }
+
+        for (String word : duplicateWords.keySet()) {
+            int count = duplicateWords.get(word);
+            System.out.println(word + ": " + count);
         }
     }
 }
